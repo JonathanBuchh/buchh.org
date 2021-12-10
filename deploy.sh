9e0cd75c3215a5c83a980b0ac056c0e5d27481ea
Jonathan Buchholz <63243849+JonathanBuchh@users.noreply.github.com>
2021-12-09 22:26:46 -0500

    Complete deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+jekyll build
+cd _site && \
+yarn add https://github.com/tmcw/premature-optimizer && \
+for file in $(find *.html)
+do
+    npx premature-optimizer $file
+done
+git add . && \
+git commit -m "Deploy _site/ to gh-pages" && \
+git push origin gh-pages && \
+cd .. && \
+git add . && \
+git commit && \
+echo "Successfully built and pushed gh-pages to GitHubH"
