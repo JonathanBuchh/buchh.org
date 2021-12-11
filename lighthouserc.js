19aa6b2dfc69f56f749577555afb324755b08c20
Jonathan Buchholz <63243849+JonathanBuchh@users.noreply.github.com>
2021-12-10 23:55:55 -0500

    Just check accessibility

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,7 +8,7 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     assert: {
-      "preset": "lighthouse:no-pwa",
+      "categories:accessibility": ["error", {"minScore": 1}]
     },
   },
 };
