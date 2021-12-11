03fb0034b58c677114bd3241c1212dcc21d8fbda
Jonathan Buchholz <63243849+JonathanBuchh@users.noreply.github.com>
2021-12-10 23:19:53 -0500

    Revert to last working GitHub workflow

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,14 +1,25 @@
+// module.exports = {
+//   ci: {
+//     collect: {
+//       staticDistDir: './_site',
+//       numberOfRuns: 1,
+//     },
+//     assert: {
+//       "categories:performance": ["error", {"minScore": 1}],
+//       "categories:accessibility": ["error", {"minScore": 1}],
+//       "categories:best-practices": ["error", {"minScore": 1}],
+//       "categories:seo": ["error", {"minScore": 1}],
+//     },
+//   },
+// };
 module.exports = {
   ci: {
     collect: {
       staticDistDir: './_site',
       numberOfRuns: 1,
     },
-    assert: {
-      "categories:performance": ["error", {"minScore": 1}],
-      "categories:accessibility": ["error", {"minScore": 1}],
-      "categories:best-practices": ["error", {"minScore": 1}],
-      "categories:seo": ["error", {"minScore": 1}],
+    upload: {
+      target: 'temporary-public-storage',
     },
   },
 };
